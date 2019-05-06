52a21bb2841748f5e4c40dd30c3b1010eeed1eaf
Alessio Paccoia <alessio.paccoia@cubbit.io>
2019-05-06 22:08:36 +0200

    Try to fix semicolumn expansion

--- cmake/utils/Environment.cmake
+++ cmake/utils/Environment.cmake
@@ -7,7 +7,9 @@ set(ENVIRONMENT_INCLUDED true)
 # Add Depot tools and $PATH to webrtc PATH
 list(APPEND _WEBRTC_PATH ${DEPOT_TOOLS_PATH} $ENV{PATH})
 
-if(UNIX)
+if(WIN32)
+    string(REGEX REPLACE ";" $<SEMICOLON> _WEBRTC_PATH "${_WEBRTC_PATH}")
+else()
     string(REGEX REPLACE ";" ":" _WEBRTC_PATH "${_WEBRTC_PATH}")
 endif()
 
