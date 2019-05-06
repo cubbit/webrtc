5229180d18500c78acf813a91c3a8580c50bfbd4
Alessio Paccoia <alessio.paccoia@cubbit.io>
2019-05-06 21:21:25 +0200

    Windows path with ;

--- cmake/utils/Environment.cmake
+++ cmake/utils/Environment.cmake
@@ -6,9 +6,12 @@ set(ENVIRONMENT_INCLUDED true)
 
 # Add Depot tools and $PATH to webrtc PATH
 list(APPEND _WEBRTC_PATH ${DEPOT_TOOLS_PATH} $ENV{PATH})
-string(REGEX REPLACE ";" ":" _WEBRTC_PATH "${_WEBRTC_PATH}")
 
-# Set custom env variable 
+if(UNIX)
+    string(REGEX REPLACE ";" ":" _WEBRTC_PATH "${_WEBRTC_PATH}")
+endif()
+
+# Set custom env variable
 set(_ENV PATH="${_WEBRTC_PATH}")
 
 # Set prefix execute
