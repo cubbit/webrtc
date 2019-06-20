b9be4d3055fc7653690c57a9ecf0aea180986468
Alessio Paccoia <alessio.paccoia@cubbit.io>
2019-06-21 01:39:04 +0200

    Fix headers copy

--- cmake/Setup.cmake
+++ cmake/Setup.cmake
@@ -3,13 +3,13 @@ if(WIN32)
     string(REGEX REPLACE "/" "\\\\" WEBRTC_LIB_SOURCE ${WEBRTC_LIB_SOURCE})
     set(WEBRTC_LIB_COPY_COMMAND robocopy ${WEBRTC_LIB_SOURCE} ${WEBRTC_LIB_FOLDER} *.lib /s /purge /lev:1 || exit 0)
 else()
-    set(WEBRTC_LIB_COPY_COMMAND cp ${WEBRTC_LIB_SOURCE}/*.a ${WEBRTC_LIB_FOLDER})
+    set(WEBRTC_LIB_COPY_COMMAND cp *.a ${WEBRTC_LIB_FOLDER})
 endif()
 
 webrtc_command(
     NAME lib-copy
     COMMAND ${WEBRTC_LIB_COPY_COMMAND}
-    WORKING_DIRECTORY ${WEBRTC_FOLDER}
+    WORKING_DIRECTORY ${WEBRTC_LIB_SOURCE}
     DEPENDS build
 )
 
@@ -18,13 +18,13 @@ if(WIN32)
     string(REGEX REPLACE "/" "\\\\" WEBRTC_HEADER_SOURCE ${WEBRTC_HEADER_SOURCE})
     set(WEBRTC_HEADERS_COPY_COMMAND robocopy ${WEBRTC_HEADER_SOURCE} ${WEBRTC_HEADERS_FOLDER} *.h /s /purge || exit 0)
 else()
-    set(WEBRTC_HEADERS_COPY_COMMAND cd ${WEBRTC_HEADER_SOURCE} && find . -name '*.h' | cpio -pdm ${WEBRTC_HEADERS_FOLDER}/)
+    set(WEBRTC_HEADERS_COPY_COMMAND find . -name '*.h' | cpio -pdm ${WEBRTC_HEADERS_FOLDER}/)
 endif()
 
 webrtc_command(
     NAME headers-copy
     COMMAND ${WEBRTC_HEADERS_COPY_COMMAND}
-    WORKING_DIRECTORY ${WEBRTC_FOLDER}
+    WORKING_DIRECTORY ${WEBRTC_HEADER_SOURCE}
     DEPENDS build
 )
 
