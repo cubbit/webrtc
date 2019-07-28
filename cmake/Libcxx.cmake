a28add29faeb68e4ec2eca448e69d67529535947
Alessio Paccoia <alessio.paccoia@cubbit.io>
2019-07-29 01:38:42 +0200

    Export built stdlib

---
+++ cmake/Libcxx.cmake
@@ -0,0 +1,64 @@
+set(LIBCPP_OBJECTS
+    algorithm.o
+    any.o
+    bind.o
+    chrono.o
+    condition_variable.o
+    debug.o
+    exception.o
+    functional.o
+    future.o
+    hash.o
+    ios.o
+    iostream.o
+    locale.o
+    memory.o
+    mutex.o
+    new.o
+    optional.o
+    random.o
+    regex.o
+    shared_mutex.o
+    stdexcept.o
+    string.o
+    strstream.o
+    system_error.o
+    thread.o
+    typeinfo.o
+    utility.o
+    valarray.o
+    variant.o
+    vector.o
+)
+list(TRANSFORM LIBCPP_OBJECTS PREPEND ${WEBRTC_LIB_SOURCE}/buildtools/third_party/libc++/libc++/)
+
+set(LIBCPPABI_OBJECTS
+    abort_message.o
+    cxa_aux_runtime.o
+    cxa_default_handlers.o
+    cxa_demangle.o
+    cxa_exception.o
+    cxa_exception_storage.o
+    cxa_guard.o
+    cxa_handlers.o
+    cxa_personality.o
+    cxa_unexpected.o
+    cxa_vector.o
+    cxa_virtual.o
+    fallback_malloc.o
+    private_typeinfo.o
+    stdlib_exception.o
+    stdlib_stdexcept.o
+    stdlib_typeinfo.o
+)
+list(TRANSFORM LIBCPPABI_OBJECTS PREPEND ${WEBRTC_LIB_SOURCE}/buildtools/third_party/libc++abi/libc++abi/)
+
+set(LIBCXX_NAME libcxx.a)
+
+webrtc_command(
+    NAME libcxx
+    COMMAND ${CMAKE_AR}
+    ARGS -r ${LIBCPP_NAME} ${LIBCPP_OBJECT} ${LIBCPPABI_OBJECTS}
+    WORKING_DIRECTORY ${WEBRTC_LIB_SOURCE}
+    DEPENDS build
+)
--- cmake/Setup.cmake
+++ cmake/Setup.cmake
@@ -1,9 +1,13 @@
 set(WEBRTC_LIB_SOURCE ${CMAKE_CURRENT_BINARY_DIR}/webrtc/src/out/${WEBRTC_BUILD_TYPE}/obj)
+
+set(WEBRTC_LIB_DEPENDS build)
 if(WIN32)
     string(REGEX REPLACE "/" "\\\\" WEBRTC_LIB_SOURCE ${WEBRTC_LIB_SOURCE})
     set(ROBOCOPY_SILENT /NFL /NDL /NJH /NJS /NC /NS /NP)
     set(WEBRTC_LIB_COPY_COMMAND robocopy ${WEBRTC_LIB_SOURCE} ${WEBRTC_LIB_FOLDER} *.lib /s /purge /lev:1 ${ROBOCOPY_SILENT} || exit 0)
 else()
+    include(Libcxx)
+    set(WEBRTC_LIB_DEPENDS libcxx)
     set(WEBRTC_LIB_COPY_COMMAND cp *.a ${WEBRTC_LIB_FOLDER})
 endif()
 
@@ -11,16 +15,16 @@ webrtc_command(
     NAME lib-copy
     COMMAND ${WEBRTC_LIB_COPY_COMMAND}
     WORKING_DIRECTORY ${WEBRTC_LIB_SOURCE}
-    DEPENDS build
+    DEPENDS ${WEBRTC_LIB_DEPENDS}
 )
 
 set(WEBRTC_HEADER_SOURCE ${CMAKE_CURRENT_BINARY_DIR}/webrtc/src/)
 if(WIN32)
     string(REGEX REPLACE "/" "\\\\" WEBRTC_HEADER_SOURCE ${WEBRTC_HEADER_SOURCE})
     set(ROBOCOPY_SILENT /NFL /NDL /NJH /NJS /NC /NS /NP)
-    set(WEBRTC_HEADERS_COPY_COMMAND robocopy ${WEBRTC_HEADER_SOURCE} ${WEBRTC_HEADERS_FOLDER} *.h /s /purge ${ROBOCOPY_SILENT} || exit 0)
+    set(WEBRTC_HEADERS_COPY_COMMAND robocopy ${WEBRTC_HEADER_SOURCE} ${WEBRTC_HEADERS_FOLDER} *.h *include* /s /purge ${ROBOCOPY_SILENT} || exit 0)
 else()
-    set(WEBRTC_HEADERS_COPY_COMMAND find . -name '*.h' | cpio -pdm ${WEBRTC_HEADERS_FOLDER}/)
+    set(WEBRTC_HEADERS_COPY_COMMAND find . \\\( -name '*.h' -o -path '*include*' \\\) | cpio -pdm ${WEBRTC_HEADERS_FOLDER}/)
 endif()
 
 webrtc_command(
